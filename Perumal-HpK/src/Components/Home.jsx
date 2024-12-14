67c78537250219b20cd4df3f0a22f378ce7e8336
PerumalH <perumalhpk250@gmail.com>
2024-12-14 23:30:03 +0530

    Update : Start the Home Page

--- Perumal-HpK/src/Components/Home.jsx
+++ Perumal-HpK/src/Components/Home.jsx
@@ -1,5 +1,34 @@
+import logo from "../assets/React+Html.png";
+import "../style/sass/layout/_home.scss";
+import hiVideo from "../assets/hi.mp4";
+import HiImg from "../assets/hi-unscreen.gif";
 const Home = () => {
-  return <div>{"{...}"}</div>;
+  return (
+    <div className="Lists">
+      <div className="list-item logo">
+        <img src={logo} alt="React=Html+JS" className="logo-img" />
+      </div>
+      <div className="list-item intro">
+        <p>{`Hi There!`}</p>
+        <h6>{"I'm Perumal Hpk"}</h6>
+        <h5>{"The Frontend Developer"}</h5>
+        <img src={HiImg} alt="hi" className="intro-img" />
+      </div>
+      <div className="list-item action">
+        <button>Explore Me</button>
+        <p>Before you Going to </p>
+        <button>Excite Me</button>
+      </div>
+      <div className="floating-symbols">
+        <span className="symbol">{"{...}"}</span>
+        <span className="symbol">{"&&"}</span>
+        <span className="symbol">{"@"}</span>
+        <span className="symbol">{"==="}</span>
+        <span className="symbol">{"||"}</span>
+      </div>
+      <div>Copyright @ HpK</div>
+    </div>
+  );
 };
 
 export default Home;
